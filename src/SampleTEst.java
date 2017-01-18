528ab87fc6e5116984521184c4762ca4926d48a1
Lavanya Kumar <lavanyakumar497@gmail.com>
2017-01-18 20:45:02 +0530

    test update

--- src/SampleTEst.java
+++ src/SampleTEst.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+
 
 
 public class SampleTEst {
 
 	
 	public static void main(String[] args) {
-		Scanner sc =  new Scanner(System.in);
-		sc.nextLine();
+		
 		
 		System.out.println("sys");
+		System.out.println("LAVANYAKUMAR");
 
 	}
 
